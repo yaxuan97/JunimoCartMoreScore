0a027f8eec114793ac8380409a0994809ea09fdf
yaxuan zhang <yaxuanz66@gmail.com>
2022-09-04 15:00:13 +0800

    添加项目文件。

---
+++ ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI;
+using StardewValley.Minigames;
+using StardewValley;
+using StardewModdingAPI.Events;
+
+namespace JunimoCartMoreScore {
+	public class ModConfig {
+		public int Multiple { get; set; } = 1;
+	}
+	public class ModEntry : Mod {
+		private ModConfig Config;
+		public override void Entry(IModHelper helper) {
+			this.Config = this.Helper.ReadConfig<ModConfig>();
+			helper.Events.GameLoop.UpdateTicked += ScoreChanger;
+		}
+		private static int _lastscore = 0;
+		private void ScoreChanger(object sender, UpdateTickedEventArgs updateTickedEventArgs){
+			if(Context.IsWorldReady){
+				if(Game1.currentMinigame is MineCart game){
+					IReflectedField<int> rscore = Helper.Reflection.GetField<int>(game, "score");
+					int score = rscore.GetValue();
+					int i = score - _lastscore;
+					if (i > 0) {
+						_lastscore += i * Config.Multiple;
+						rscore.SetValue(_lastscore);
+					} else if (i == 0) { } else { 
+						_lastscore = 0;
+					}
+				}
+			}
+		}
+	}
+}
